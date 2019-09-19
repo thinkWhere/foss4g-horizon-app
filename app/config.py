2fb225ab5e79bdd0e520394af5ed5f3c98e83378
Alastair Carson <alastair.carson@thinkwhere.com>
2019-09-19 18:04:13 +0100

    Make config easier for the day

--- app/config.py
+++ app/config.py
@@ -1,9 +1,18 @@
 
 class Config:
-    # TODO AC to comment what these keys are
+    # S3 Access Credentials
     S3_ACCESS_KEY = ""
     S3_ACCESS_SECRET_KEY = ""
-    DATABASE_CONNECTION_STRING = ""
+    # Database Connection String
+    DATABASE_CONNECTION_STRING = """
+        host=''
+        port='5432'
+        dbname='appsstaging'
+        user='viewpoint_user'
+        password='viewpoint'
+        """
+    # S3 Bucket for the Web Application and Output Files
     VIEWER_BUCKET = ""
-    # Public bucket that thinkWhere host (temporarily) data available from Ordnance Survey
-    TERRAIN_DATA_BUCKET = ""
+    # Terrain Data S3 Bucket
+    # (Public bucket that thinkWhere host (temporarily) data available from Ordnance Survey)
+    TERRAIN_DATA_BUCKET = "ac-foss4g-terrain-data"
